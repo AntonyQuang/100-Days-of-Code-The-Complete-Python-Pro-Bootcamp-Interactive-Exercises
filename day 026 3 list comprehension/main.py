6395d39b015d6081d22c9f83fbf573446d106738
Antony Quang <antony.quang@googlemail.com>
2022-03-08 22:03:03 +0000

    day 026 exercises

---
+++ day 026 3 list comprehension/main.py
@@ -0,0 +1,12 @@
+with open("file1.txt") as file1:
+    file1_data = file1.readlines()
+    
+with open("file2.txt") as file2:
+    file2_data = file2.readlines()
+
+result = [int(number.strip("\n")) for number in file1_data if number in file2_data]
+# Write your code above 👆
+
+print(result)
+
+
